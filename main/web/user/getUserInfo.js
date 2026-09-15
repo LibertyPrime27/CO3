b08b83ab5784a8f170d00de049190ee2e0f77d08
tbvns <tbvns601@gmail.com>
2026-09-15 22:11:18 +0200

    Fixed avatar when logged in, fixes second part of #132 (The first part was patched in the last commit)

--- main/web/user/getUserInfo.js
+++ main/web/user/getUserInfo.js
@@ -6,21 +6,21 @@ async function scrapeUserPage(url, username) {
   const res = await getUrl(url);
 
   const doc = new DomParser().parseFromString(res, "text/html");
-  const avatar = Array.from(doc.getElementsByTagName("img")).filter(img => img?.getAttribute("class") === "icon")[0];
+  const avatarList = Array.from(doc.getElementsByTagName("img")).filter(img => img?.getAttribute("class") === "icon");
+  const avatar = avatarList[avatarList.length - 1];
   const bio = Array.from(doc.getElementsByTagName("blockquote"))
     .filter(a => a.getAttribute("class") === `userstuff` && a.parentNode.getAttribute("id") !== "admin-banner")[0];
 
   const meta = Array.from(doc.getElementsByTagName("dl"))
     .filter(a => a.getAttribute("class") === `meta`)[0];
-  console.log(meta);
   const joinDate = meta?.childNodes[7]?.textContent;
-
-  console.log(avatar);
 
   let avatarUrl = avatar.getAttribute("src");
   if (avatarUrl === "/images/skins/iconsets/default/icon_user.png") {
     avatarUrl = "https://archiveofourown.org/images/skins/iconsets/default/icon_user.png";
   }
+
+  console.log(doc);
 
   return {
     username: username,
